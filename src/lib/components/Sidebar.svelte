5426a16aae7dd15bc8bc203b09ddd34b021cb0ed
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-05-24 13:08:23 +0200

    refactor(sidebar): 🔨 isolate component

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import type { Note } from '$lib/server/db/schema';
+	import { Search, SortAsc } from 'lucide-svelte';
 	import type { Snippet } from 'svelte';
+	import IconPicker from './IconPicker.svelte';
 
 	interface Props {
-		children?: Snippet;
-		header: Snippet;
+		notes: Note[];
+		activeNote: Note;
 	}
 
-	let { children, header }: Props = $props();
+	let { notes, activeNote = $bindable() }: Props = $props();
+
+	function handleNoteClick(event: MouseEvent) {
+		const id = (event.currentTarget as HTMLButtonElement).value;
+		const note = notes.find((note) => note.id === id);
+
+		if (note) activeNote = note;
+	}
 </script>
 
 <div
@@ -14,7 +24,60 @@
             z-50 flex flex-col transition-colors ease-modern"
 >
 	<div class="flex flex-col mt-14">
-		{@render header?.()}
-		{@render children?.()}
+		<button class="muted" onclick={() => alert('TODO:')}>
+			<Search size={16} />
+			<span>Search</span>
+		</button>
+		<button class="muted" onclick={() => alert('TODO:')}>
+			<SortAsc size={16} />
+			<span>Sort</span>
+		</button>
+
+		{#each notes as note (note.id)}
+			<button
+				class:active={note.id === activeNote.id}
+				onclick={handleNoteClick}
+				aria-label={note.title}
+				title={note.title}
+				value={note.id}
+			>
+				<IconPicker bind:selectedIcon={note.icon} />
+				<span
+					bind:textContent={note.title}
+					spellcheck="false"
+					class="outline-none caret-accent"
+					contenteditable
+				></span>
+			</button>
+		{/each}
 	</div>
 </div>
+
+<style lang="postcss">
+	@reference "../../app.css";
+
+	button {
+		@apply bg-transparent text-fg-primary border-none 
+           m-1.5 p-2 rounded-md cursor-pointer transition-all
+           ease-modern text-xs font-normal font-display 
+           text-left flex items-center gap-2;
+
+		@apply hover:bg-surface-secondary hover:text-fg-primary;
+	}
+
+	button.active {
+		@apply bg-surface-secondary text-fg-primary;
+	}
+
+	span {
+		@apply text-ellipsis overflow-hidden whitespace-nowrap flex-1 min-w-0;
+	}
+
+	.muted {
+		@apply [&>*]:text-fg-secondary [&>*]:font-light;
+	}
+
+	.muted:hover {
+		@apply [&>*]:text-fg-primary [&>*]:font-normal;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import Editor from '$lib/components/editor/Editor.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
-	import { Search, SortAsc } from 'lucide-svelte';
 	import type { PageData } from './$types';
-	import IconPicker from '$lib/components/IconPicker.svelte';
 	import { debounce } from '$lib';
 	import equal from 'fast-deep-equal';
 	import type { Note } from '$lib/server/db/schema';
@@ -19,13 +17,6 @@
 
 	let notes = $state(data.notes);
 	let activeNote = $state(notes[0]);
-
-	function handleNoteClick(event: MouseEvent) {
-		const id = (event.currentTarget as HTMLButtonElement).value;
-		const note = notes.find((note) => note.id === id);
-
-		if (note) activeNote = note;
-	}
 
 	const save = debounce(async () => {
 		const changed: Note[] = notes.filter((note, i) => !equal(note, data.notes[i]));
@@ -51,66 +42,8 @@
 	});
 </script>
 
-<Sidebar>
-	{#snippet header()}
-		<button class="muted" onclick={() => alert('TODO:')}>
-			<Search size={16} />
-			<span>Search</span>
-		</button>
-		<button class="muted" onclick={() => alert('TODO:')}>
-			<SortAsc size={16} />
-			<span>Sort</span>
-		</button>
-	{/snippet}
-
-	{#each notes as note (note.id)}
-		<button
-			class:active={note.id === activeNote.id}
-			onclick={handleNoteClick}
-			aria-label={note.title}
-			title={note.title}
-			value={note.id}
-		>
-			<IconPicker bind:selectedIcon={note.icon} />
-			<span
-				bind:textContent={note.title}
-				spellcheck="false"
-				class="outline-none caret-accent"
-				contenteditable
-			></span>
-		</button>
-	{/each}
-</Sidebar>
+<Sidebar {notes} bind:activeNote={activeNote} />
 
 <div class="flex-col w-full h-full pl-40">
 	<Editor bind:note={activeNote} />
 </div>
-
-<style lang="postcss">
-	@reference "../app.css";
-
-	button {
-		@apply bg-transparent text-fg-primary border-none 
-           m-1.5 p-2 rounded-md cursor-pointer transition-all
-           ease-modern text-xs font-normal font-display 
-           text-left flex items-center gap-2;
-
-		@apply hover:bg-surface-secondary hover:text-fg-primary;
-	}
-
-	button.active {
-		@apply bg-surface-secondary text-fg-primary;
-	}
-
-	span {
-		@apply text-ellipsis overflow-hidden whitespace-nowrap flex-1 min-w-0;
-	}
-
-	.muted {
-		@apply [&>*]:text-fg-secondary [&>*]:font-light;
-	}
-
-	.muted:hover {
-		@apply [&>*]:text-fg-primary [&>*]:font-normal;
-	}
-</style>
